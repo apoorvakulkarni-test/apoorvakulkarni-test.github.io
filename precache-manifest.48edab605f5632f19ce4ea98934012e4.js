444a9756bd4c724a29b3708c47c69b494f690b7a
Chris Phan <nhatgau0@gmail.com>
2019-09-11 00:06:27 -0400

    Updates

---
+++ precache-manifest.48edab605f5632f19ce4ea98934012e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390b396dd0e2f547acd08ac92817fc61",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/index.html"
+  },
+  {
+    "revision": "f007c551440e401abac4",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/css/main.2025a2db.chunk.css"
+  },
+  {
+    "revision": "df494f6bfa786262c2b6",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/js/2.849c0586.chunk.js"
+  },
+  {
+    "revision": "f007c551440e401abac4",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/js/main.f32d5fbc.chunk.js"
+  },
+  {
+    "revision": "e4ad1fc4489e1bbde383",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/js/runtime~main.630bc0b4.js"
+  },
+  {
+    "revision": "657ba9a9552f300a8dc6c763ab726770",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/JosefinSans-Light.657ba9a9.ttf"
+  },
+  {
+    "revision": "673965c55e0656d563fb1ff8200bbf86",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/JosefinSans-Regular.673965c5.ttf"
+  },
+  {
+    "revision": "9e9720b4ea350523d1fc10da0254204e",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/logo.9e9720b4.png"
+  },
+  {
+    "revision": "8450d192d237423fe34ec6f66720df15",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/tile-comic.8450d192.png"
+  },
+  {
+    "revision": "56fc4bb94066c143468c45103ae6817b",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/tile-moonboat.56fc4bb9.png"
+  },
+  {
+    "revision": "7ff96b9aa3b79fbcdae2c78c5fba1e72",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/tile-ufb.7ff96b9a.jpg"
+  },
+  {
+    "revision": "d8b938d7b22dea5f8f4fd5f991794f79",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/tile-werth.d8b938d7.png"
+  },
+  {
+    "revision": "5afe55b5e14eb1890123229bee70a032",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/tiles-mural.5afe55b5.png"
+  },
+  {
+    "revision": "4f3d86dd595866eff24bbdb69514e1c7",
+    "url": "/apoorvakulkarni-test/apoorvakulkarni-test/static/media/tiles-plu.4f3d86dd.png"
+  }
+]);
