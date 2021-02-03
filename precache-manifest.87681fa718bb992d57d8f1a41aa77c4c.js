8e3f7cb31e20e0e4d81e8fbe09e930777c0a1263
Chris Phan <nhat.phan@uconn.edu>
2021-02-02 19:05:29 -0500

    New Commit

---
+++ precache-manifest.87681fa718bb992d57d8f1a41aa77c4c.js
@@ -0,0 +1,410 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72889aa8c6b3e61863d329d4ae815f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8213830d199d2053c07",
+    "url": "/static/css/main.27b842dd.chunk.css"
+  },
+  {
+    "revision": "b878ee71677a0cab7f0a",
+    "url": "/static/js/2.be27d61c.chunk.js"
+  },
+  {
+    "revision": "b8213830d199d2053c07",
+    "url": "/static/js/main.165996e6.chunk.js"
+  },
+  {
+    "revision": "a63bed7ad542dc7574d1",
+    "url": "/static/js/runtime~main.f841a439.js"
+  },
+  {
+    "revision": "657ba9a9552f300a8dc6c763ab726770",
+    "url": "/static/media/JosefinSans-Light.657ba9a9.ttf"
+  },
+  {
+    "revision": "673965c55e0656d563fb1ff8200bbf86",
+    "url": "/static/media/JosefinSans-Regular.673965c5.ttf"
+  },
+  {
+    "revision": "401e74ae9d65f1d3c578aebadb513b64",
+    "url": "/static/media/MuralDisplaytwo.401e74ae.JPG"
+  },
+  {
+    "revision": "a774cb27b629b7b22871ed012e2b6c65",
+    "url": "/static/media/Project-descriptions.a774cb27.docx"
+  },
+  {
+    "revision": "0f18d3f40f29e731232d202cd78c462f",
+    "url": "/static/media/YesevaOne-Regular.0f18d3f4.ttf"
+  },
+  {
+    "revision": "58c2f50f3ff7e4259a076e7802a873e5",
+    "url": "/static/media/apoorva.58c2f50f.gif"
+  },
+  {
+    "revision": "f5f2dd8adb306b37790076de46cad6c9",
+    "url": "/static/media/cs.f5f2dd8a.gif"
+  },
+  {
+    "revision": "68e92323a5a87ae2ebfbb308287a73ca",
+    "url": "/static/media/dude-werthtower.68e92323.png"
+  },
+  {
+    "revision": "c2c2ff2daab84dc6ad6272f0c3286cbd",
+    "url": "/static/media/football.c2c2ff2d.png"
+  },
+  {
+    "revision": "79edcb591e43a96e1fc304c6e7958bd5",
+    "url": "/static/media/friendly.79edcb59.jpg"
+  },
+  {
+    "revision": "37a98c0e5cf50086d7982b00c9e6ebd8",
+    "url": "/static/media/graphicFive.37a98c0e.png"
+  },
+  {
+    "revision": "ab9dd0923dfd79aa49da08228b41d2a6",
+    "url": "/static/media/graphicFour.ab9dd092.png"
+  },
+  {
+    "revision": "c53a813abc903dfd377042b4ceb68946",
+    "url": "/static/media/graphicOne.c53a813a.png"
+  },
+  {
+    "revision": "e9fb5b2425ebd512aff411a0671e53ea",
+    "url": "/static/media/graphicSeven.e9fb5b24.png"
+  },
+  {
+    "revision": "c432e8352f2f462be9ebbf4a08948ee0",
+    "url": "/static/media/graphicSix.c432e835.png"
+  },
+  {
+    "revision": "3520387b7b7e5ee1f6d17eebe0e6f612",
+    "url": "/static/media/graphicThree.3520387b.png"
+  },
+  {
+    "revision": "1571deb2d914db2245eea05b511b6b81",
+    "url": "/static/media/graphicTwo.1571deb2.png"
+  },
+  {
+    "revision": "81d06f30d6045f06e6c275b98e76f4c6",
+    "url": "/static/media/illusion.81d06f30.png"
+  },
+  {
+    "revision": "9e9720b4ea350523d1fc10da0254204e",
+    "url": "/static/media/logo.9e9720b4.png"
+  },
+  {
+    "revision": "e045168441d554f53b2421cdc9b55c88",
+    "url": "/static/media/logodreamwork.e0451684.jpg"
+  },
+  {
+    "revision": "d8dc4d0c9716cf395eaae72368f0e4bf",
+    "url": "/static/media/logodreamworkmoon.d8dc4d0c.png"
+  },
+  {
+    "revision": "0332d5d5a930ef07ffb68d9a264d4fca",
+    "url": "/static/media/mb1.0332d5d5.png"
+  },
+  {
+    "revision": "daf270e6adc86b7fbc102260d26e85eb",
+    "url": "/static/media/mb2.daf270e6.png"
+  },
+  {
+    "revision": "495364a4b6d0cf2a4ae00c59fb2995b5",
+    "url": "/static/media/mb3.495364a4.png"
+  },
+  {
+    "revision": "e6c46d2e87a2d0965bd27b3aed64a75f",
+    "url": "/static/media/mb4.e6c46d2e.png"
+  },
+  {
+    "revision": "d07618fccb732ad585d0c5d4e27cd0ef",
+    "url": "/static/media/mb5.d07618fc.png"
+  },
+  {
+    "revision": "e090638424bef7f801041a47cc2b5392",
+    "url": "/static/media/moment.e0906384.jpg"
+  },
+  {
+    "revision": "c9bdaa123ead6cb5d778d10441688f0d",
+    "url": "/static/media/motto-0.c9bdaa12.png"
+  },
+  {
+    "revision": "e3a636a6631457deba2de48f45492626",
+    "url": "/static/media/motto-1.e3a636a6.png"
+  },
+  {
+    "revision": "c99da76b401d7889dcb617b1364e7f3d",
+    "url": "/static/media/motto-2.c99da76b.png"
+  },
+  {
+    "revision": "764e3a820315b50a25a34b95328a3661",
+    "url": "/static/media/motto-3.764e3a82.PNG"
+  },
+  {
+    "revision": "c99da76b401d7889dcb617b1364e7f3d",
+    "url": "/static/media/motto-4.c99da76b.png"
+  },
+  {
+    "revision": "109798e5e1f737dfd89ee8121d60a3ae",
+    "url": "/static/media/motto-5.109798e5.png"
+  },
+  {
+    "revision": "f18f126bd4bd14f0d9a500e99784a574",
+    "url": "/static/media/motto-6.f18f126b.png"
+  },
+  {
+    "revision": "af4d13dfbddc01b1a6cbccf7a2efdb47",
+    "url": "/static/media/motto-7.af4d13df.png"
+  },
+  {
+    "revision": "03201a7031f4d1799e25daa0a3d9c190",
+    "url": "/static/media/motto-8.03201a70.png"
+  },
+  {
+    "revision": "c87d22fc83b7c451a9392ae367dd7237",
+    "url": "/static/media/mural-colored-2.c87d22fc.jpg"
+  },
+  {
+    "revision": "9cf4ec0a86b1cde39a80f7bda9c8aed3",
+    "url": "/static/media/mural-colored.9cf4ec0a.jpg"
+  },
+  {
+    "revision": "8c744adb667c819cc56a3e9028c086f9",
+    "url": "/static/media/muralDisplay.8c744adb.JPG"
+  },
+  {
+    "revision": "4caef3643b91f721045c683486e20159",
+    "url": "/static/media/muralsketch.4caef364.jpg"
+  },
+  {
+    "revision": "96bf58f349b91f9f6185d04377c0f560",
+    "url": "/static/media/park.96bf58f3.png"
+  },
+  {
+    "revision": "dd8b5f832e6b24c8ebec54075966e7b0",
+    "url": "/static/media/parkupColoredLogo.dd8b5f83.png"
+  },
+  {
+    "revision": "d0caaf0dd112f2e41785912c502b579f",
+    "url": "/static/media/parkupColors.d0caaf0d.jpg"
+  },
+  {
+    "revision": "96bf58f349b91f9f6185d04377c0f560",
+    "url": "/static/media/parkupFirstPic.96bf58f3.png"
+  },
+  {
+    "revision": "af4738fc7d0d17e966721e7f4d693ade",
+    "url": "/static/media/parkupInitialLogo.af4738fc.jpg"
+  },
+  {
+    "revision": "f14029984f2754656024725eb2e31548",
+    "url": "/static/media/parkupIsometric.f1402998.png"
+  },
+  {
+    "revision": "572b4c1a615f2871aae15e02b9734401",
+    "url": "/static/media/parkupLowFidelity.572b4c1a.jpg"
+  },
+  {
+    "revision": "49ba0f96a61d1b1308d60b51b7bf7c12",
+    "url": "/static/media/parkupTable.49ba0f96.png"
+  },
+  {
+    "revision": "0097d9b0b27291fd63ca83cd3819c00b",
+    "url": "/static/media/parkupTransparentLogo.0097d9b0.png"
+  },
+  {
+    "revision": "5260bf9c0fd1863a939735574fe08569",
+    "url": "/static/media/parkupUserFlow.5260bf9c.jpg"
+  },
+  {
+    "revision": "9ef10e2edb779e84f7438c996a296424",
+    "url": "/static/media/parkupUserResearchSlideOne.9ef10e2e.jpg"
+  },
+  {
+    "revision": "0033ad7f1bbaf06c351d4de287b10ee0",
+    "url": "/static/media/parkupUserResearchSlideThree.0033ad7f.jpg"
+  },
+  {
+    "revision": "6b8d9a4e832c2ab6c75da47561287333",
+    "url": "/static/media/parkupUserResearchSlideTwo.6b8d9a4e.jpg"
+  },
+  {
+    "revision": "f7ec4df85095337fdb80917528c05a0f",
+    "url": "/static/media/parkupWireFrame.f7ec4df8.jpg"
+  },
+  {
+    "revision": "46135ad70583eb8a632ecc5f9a716d8d",
+    "url": "/static/media/resume.46135ad7.pdf"
+  },
+  {
+    "revision": "e9afade3bac92dab9b78ad6f923368b6",
+    "url": "/static/media/riderEight.e9afade3.png"
+  },
+  {
+    "revision": "b3e96f92f0fc5175213373bacd69aff7",
+    "url": "/static/media/riderEleven.b3e96f92.png"
+  },
+  {
+    "revision": "18ad132672d1ae31095dd730602ce52a",
+    "url": "/static/media/riderFive.18ad1326.png"
+  },
+  {
+    "revision": "b317ff7f4de0d790bddc368403401819",
+    "url": "/static/media/riderFour.b317ff7f.png"
+  },
+  {
+    "revision": "ecda80e08afac8dd353d76261ab34455",
+    "url": "/static/media/riderNine.ecda80e0.png"
+  },
+  {
+    "revision": "4b74ce6efe213a4ad945915b4ba8571d",
+    "url": "/static/media/riderOne.4b74ce6e.png"
+  },
+  {
+    "revision": "c00440eec5648b61beba69d2519a77b2",
+    "url": "/static/media/riderSeven.c00440ee.png"
+  },
+  {
+    "revision": "87a5605ea0349ccd0c3094a46f28bc39",
+    "url": "/static/media/riderSix.87a5605e.png"
+  },
+  {
+    "revision": "cc4915f73c6669490c2a097bcceec468",
+    "url": "/static/media/riderTen.cc4915f7.png"
+  },
+  {
+    "revision": "52fef581c285b4627a01a50dd0cfe022",
+    "url": "/static/media/riderThree.52fef581.png"
+  },
+  {
+    "revision": "17724b97c5e5f291349f8d3c5a0b1260",
+    "url": "/static/media/riderTwelve.17724b97.png"
+  },
+  {
+    "revision": "d56335668908e6e139132684a281cbb8",
+    "url": "/static/media/riderTwo.d5633566.png"
+  },
+  {
+    "revision": "496bfbc914310215479529775754e7e6",
+    "url": "/static/media/sg1.496bfbc9.jpg"
+  },
+  {
+    "revision": "316c471ee229227f4ef2b20b33a59e82",
+    "url": "/static/media/sg2.316c471e.jpg"
+  },
+  {
+    "revision": "cce3099a9bebcab4b20d7d5d999cb2c5",
+    "url": "/static/media/sg3.cce3099a.jpg"
+  },
+  {
+    "revision": "b63a0f79d45e7a0ca5c9c2821bd46287",
+    "url": "/static/media/sg4.b63a0f79.jpg"
+  },
+  {
+    "revision": "b6f019477e2de289656d1e3aaf1d393b",
+    "url": "/static/media/sgz1.b6f01947.png"
+  },
+  {
+    "revision": "56321fd64e30809547fb719b65ea4f73",
+    "url": "/static/media/sgz2.56321fd6.png"
+  },
+  {
+    "revision": "9063d00e77db2aa23955399283dcd91e",
+    "url": "/static/media/tile-ar.9063d00e.png"
+  },
+  {
+    "revision": "05ce60c33ef9c316d448d95692fdc132",
+    "url": "/static/media/tile-comic.05ce60c3.jpg"
+  },
+  {
+    "revision": "4ec2a9685450006ec6efb114096f4a45",
+    "url": "/static/media/tile-graphic.4ec2a968.png"
+  },
+  {
+    "revision": "8573ee3e19e961a20cf3a812975568e1",
+    "url": "/static/media/tile-moonboat.8573ee3e.jpg"
+  },
+  {
+    "revision": "e83116b91af0c57c431ac9561c54626a",
+    "url": "/static/media/tile-motiongraphic.e83116b9.jpg"
+  },
+  {
+    "revision": "e83116b91af0c57c431ac9561c54626a",
+    "url": "/static/media/tile-motiongraphic.jpg.e83116b9.jpg"
+  },
+  {
+    "revision": "c0fadbd353ccded3bbe7ea8438d4604e",
+    "url": "/static/media/tile-moto.c0fadbd3.png"
+  },
+  {
+    "revision": "e471e256ec730f3c729bb34029b92465",
+    "url": "/static/media/tile-mural.e471e256.jpg"
+  },
+  {
+    "revision": "fcf229f5d37eb7aa9f8670ad477f2afc",
+    "url": "/static/media/tile-mural.fcf229f5.png"
+  },
+  {
+    "revision": "bb889f1828c6fe5d32b68614ca3d4e26",
+    "url": "/static/media/tile-parkup.bb889f18.png"
+  },
+  {
+    "revision": "85ef11f58023b13ceb42b888a16a0e04",
+    "url": "/static/media/tile-plu.85ef11f5.jpg"
+  },
+  {
+    "revision": "633857a97590b896e19f35ac0dee37bd",
+    "url": "/static/media/tile-rider.633857a9.png"
+  },
+  {
+    "revision": "516d700be6664ad7bb13e6dba6552e60",
+    "url": "/static/media/tile-uconnsurvive.516d700b.png"
+  },
+  {
+    "revision": "2013b3c54f18ef15035b0f6cee0c3bdf",
+    "url": "/static/media/tile-ufb.2013b3c5.jpg"
+  },
+  {
+    "revision": "e8c56d1d0d3774ea2098d6e4e832f51b",
+    "url": "/static/media/tile-werth.e8c56d1d.jpg"
+  },
+  {
+    "revision": "1e1e449f4e022b228606a9d33371be2d",
+    "url": "/static/media/usc-1.1e1e449f.jpg"
+  },
+  {
+    "revision": "8276c9f25df5cf882e465a23aed2334e",
+    "url": "/static/media/usc-1.8276c9f2.png"
+  },
+  {
+    "revision": "d7624ea02d96c8c34d6ff16de1d8ab42",
+    "url": "/static/media/usc-1.d7624ea0.gif"
+  },
+  {
+    "revision": "a56d0eb84674b173bdaf011bef7a5b73",
+    "url": "/static/media/usc-2.a56d0eb8.png"
+  },
+  {
+    "revision": "dae43cbf84713fcb52eb06f363bf8731",
+    "url": "/static/media/usc-2.dae43cbf.gif"
+  },
+  {
+    "revision": "1abb605c9df83f34c0a1e441290b1b0b",
+    "url": "/static/media/usc-3.1abb605c.gif"
+  },
+  {
+    "revision": "97c09ffd9b92e497aea7cab09a22129a",
+    "url": "/static/media/usc-3.97c09ffd.jpg"
+  },
+  {
+    "revision": "46b03331ee3b2825f888b9a78c6dcbaf",
+    "url": "/static/media/usc-4.46b03331.png"
+  },
+  {
+    "revision": "57ccfa60bb9b0d24e2e18b0960024818",
+    "url": "/static/media/usc-5.57ccfa60.jpg"
+  }
+]);
